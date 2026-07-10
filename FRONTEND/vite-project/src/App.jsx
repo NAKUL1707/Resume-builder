1d6ba8dfa896e42af3c2fe86b04c7d7d5a054150
Nakul Sharma <nakul.172005@gmail.com>
2026-07-10 14:11:11 +0530

    changes made for upgrade

--- FRONTEND/vite-project/src/App.jsx
+++ FRONTEND/vite-project/src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Background3D from "./Components/Background3D";
 import AppRoute from "./Routes/Approute";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
   return (
     <>
       <Background3D />
--- FRONTEND/vite-project/src/main.jsx
+++ FRONTEND/vite-project/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-    </BrowserRouter>
-  
+  </BrowserRouter>
 )
